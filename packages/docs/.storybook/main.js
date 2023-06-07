6159cfbdb28039fbb2c2a636f59034bda93d9af4
bernardo <beefreguglia@gmail.com>
2023-06-07 18:38:35 -0300

    fix: Add subpath to storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -17,5 +17,11 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = '/churrasco-ui/'
+    }
+    return config
+  }
 };
 export default config;
