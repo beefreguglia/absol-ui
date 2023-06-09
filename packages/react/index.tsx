3547c5ae0852f7c388dcab9fe274bce3da556770
bernardo <beefreguglia@gmail.com>
2023-06-09 13:59:51 -0300

    fix: new export package

---
+++ packages/react/index.tsx
@@ -0,0 +1,18 @@
+export * from './src/components/Box';
+export * from './src/components/Text';
+export * from './src/components/Heading';
+export * from './src/components/Avatar';
+export * from './src/components/Button';
+export * from './src/components/ScrollArea';
+export * from './src/components/Modal';
+export * from './src/components/Select';
+export {
+  config,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  styled,
+  theme,
+} from './src/styles';
